2a5dd3a185175d3f27dc39aa65dda5cab03d5136
Fumi <fumi@shoplineapp.com>
2024-02-26 04:51:11 +0800

    feat(file_object): add examination for the key value

--- app/models/file_object.rb
+++ app/models/file_object.rb
@@ -4,5 +4,5 @@ class FileObject < ApplicationRecord
   belongs_to :bucket
   has_one_attached :file
 
-  validates :key, presence: true, uniqueness: { scope: :bucket_id }
+  validates :key, presence: true, uniqueness: { scope: :bucket_id }, format: { with: /\A[A-Za-z0-9_\-\/.]+\z/ }
 end
--- spec/models/file_object_spec.rb
+++ spec/models/file_object_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe FileObject, type: :model do
@@ -7,4 +5,6 @@ RSpec.describe FileObject, type: :model do
   it { should have_one_attached(:file) }
   it { should validate_presence_of(:key) }
   it { should validate_uniqueness_of(:key).scoped_to(:bucket_id) }
+  it { should allow_value('valid_key').for(:key) }
+  it { should_not allow_value('invalid key').for(:key) }
 end
